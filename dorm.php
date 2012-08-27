f78436ead4d5467ba077b49fc75cda8650496b35
mkfsn <p408865@gmail.com>
2012-08-28 03:15:40 +0800

    排版

--- dorm.php
+++ dorm.php
@@ -56,9 +56,7 @@ function printDormIPPage($selDorm){
 	</div>
 </div>
 
-<div class="container-fluid">
-	<div class="row-fluid">
-		<div class="span10 well" id="iptable">
+<div class="span10 well" id="iptable">
 <?php 
 		$dormList = array('1','2','3','4','A','B','C','D','E','F','G','H','L');
 		if(in_array($selDorm,$dormList)){
@@ -67,25 +65,24 @@ function printDormIPPage($selDorm){
 			'</pre>';
 		}else{
 ?>
-			<div class="row">
-				<div class="span7">
-					<img src="/2006/title.jpg" class="img-polaroid" />
-				</div>
-			</div>
-			<div class="row">
-				<div class="span5">
-					<pre>
+	<div class="row">
+		<div class="span7">
+			<img src="/2006/title.jpg" class="img-polaroid" />
+		</div>
+	</div>
+	<div class="row">
+		<div class="span5">
+			<pre>
 <strong>CDPA 簡介</strong>
     CDPA，一群熱心宿網的同學所組成的「中山校園宿舍網路推廣協會」，簡稱「中山網推會」， 是學生組織。 
     英文簡稱 NSYSU-CDPA。
     為提供同學更好的宿網品質，由本協會負責宿舍網路的服務， 以維持網路的品質並減少計中實際管理上的困難。
-					</pre>
-				</div>
-			</div>
+			</pre>
+		</div>
+	</div>
 <?php
 		}
 ?>
-	</div>
 </div>
 <?php
 }
